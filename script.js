7795c0880f7ae59adc8d89e7d3893b2b76b0137b
FrankoticSloth <jslovak97@gmail.com>
2022-05-13 23:54:02 +0200

    done the ui

--- script.js
+++ script.js
@@ -27,14 +27,20 @@ function round(funA, funB) {
     (funA == 'ROCK' && funB == 'SCISSORS') || 
     (funA == 'PAPER' && funB == 'ROCK') || 
     (funA == 'SCISSORS' && funB == 'PAPER')) {
-        playerJavaScore++
-        roundOutcome = 'won'
+        setTimeout(() => {
+            playerJavaScore++
+            windows.style.backgroundColor = '#1c8b4e';
+            roundOutcome = 'won'
+        }, '1000');
     } else if (
     (funA == 'ROCK' && funB == 'PAPER') || 
     (funA == 'PAPER' && funB == 'SCISSORS') || 
     (funA == 'SCISSORS' && funB == 'ROCK')) {
-        computerJavaScore++
-        roundOutcome = 'lost'
+        setTimeout(() => {
+            computerJavaScore++
+            windows.style.backgroundColor = '#8b1e25';
+            roundOutcome = 'lost'
+        }, '1000');
     }
 }
 
@@ -50,10 +56,14 @@ console.log('CPUs score: ' + computerScore);
 
 function gameCheck() {
     if (playerJavaScore === 5) {
-        alert('You Won!');
+        game.style.display = 'none';
+        windows.style.backgroundColor = '#2cda7a';
+        won.style.display = 'block';
         restart();
     } else if (computerJavaScore === 5) {
-        alert('You Lost!');
+        game.style.display = 'none';
+        windows.style.backgroundColor = '#da2c38';
+        lost.style.display = 'block';
         restart();
     } else {
         
@@ -61,53 +71,132 @@ function gameCheck() {
 }
 
 function restart() {
-    playerJavaScore = 0;
-    computerJavaScore = 0;
-    playerScore.textContent = playerJavaScore;
-    computerScore.textContent = computerJavaScore;
+    rockButton.style.display = 'none';
+    rockOff.style.display = 'block';
+    paperButton.style.display = 'none';
+    gameRestart.style.display = 'block';
+    scissorsButton.style.display = 'none';
+    scissorsOff.style.display = 'block';
+}
+
+function turnOffIcons() {
+    lr.style.display = 'none';
+    lp.style.display = 'none';
+    ls.style.display = 'none';
+    rr.style.display = 'none';
+    rp.style.display = 'none';
+    rs.style.display = 'none';
+}
+
+function cpuIcon() {
+    if (computerPlay == 'ROCK') {
+        rr.style.display = 'block';
+    } else if (computerPlay == 'PAPER') {
+        rp.style.display = 'block';
+    } else if (computerPlay == 'SCISSORS') {
+        rs.style.display = 'block';
+    }
 }
 
 const rockButton = document.querySelector('.rock-button');
 const paperButton = document.querySelector('.paper-button');
 const scissorsButton = document.querySelector('.scissors-button');
+const rockOff = document.querySelector('.rock-off')
+const scissorsOff = document.querySelector('.scissors-off')
 playerScore = document.querySelector('.player-score');
 computerScore = document.querySelector('.computer-score');
-const startGame = document.querySelector('start-game');
-const restartGame = document.querySelector('.restart');
+const gameStart = document.querySelector('.start');
+const gameRestart = document.querySelector('.restart');
+const windows = document.querySelector('.window');
+const won = document.querySelector('.won');
+const lost = document.querySelector('.lost');
+const game = document.querySelector('.game');
+const rps = document.querySelector('.rps');
+const lr = document.querySelector('#left-rock');
+const lp = document.querySelector('#left-paper');
+const ls = document.querySelector('#left-scissors');
+const rr = document.querySelector('#right-rock');
+const rp = document.querySelector('#right-paper');
+const rs = document.querySelector('#right-scissors');
 
 
 rockButton.addEventListener('click', () => {
+    windows.style.backgroundColor = '#363636';
     playerPlay = 'ROCK';
+    turnOffIcons();
+    lr.style.display = 'block';
     computerSelect();
     computerPlay = computerSelect();
+    setTimeout(cpuIcon, '500');
     round(playerPlay, computerPlay);
-    console.log(roundOutcome);
-    playerScore.textContent = playerJavaScore;
-    computerScore.textContent = computerJavaScore;
-    gameCheck();
+    setTimeout(() => {
+        console.log(roundOutcome);
+        playerScore.textContent = playerJavaScore;
+        computerScore.textContent = computerJavaScore;
+        gameCheck();
+    }, '1010');
 });
 
 paperButton.addEventListener('click', () => {
+    windows.style.backgroundColor = '#363636';
     playerPlay = 'PAPER';
+    turnOffIcons();
+    lp.style.display = 'block';
     computerSelect();
     computerPlay = computerSelect();
+    setTimeout(cpuIcon, '500');
     round(playerPlay, computerPlay);
-    console.log(roundOutcome);
-    playerScore.textContent = playerJavaScore;
-    computerScore.textContent = computerJavaScore;
-    gameCheck();
+    setTimeout(() => {
+        console.log(roundOutcome);
+        playerScore.textContent = playerJavaScore;
+        computerScore.textContent = computerJavaScore;
+        gameCheck();
+    }, '1010');
 });
 
 scissorsButton.addEventListener('click', () => {
+    windows.style.backgroundColor = '#363636';
     playerPlay = 'SCISSORS';
+    turnOffIcons();
+    ls.style.display = 'block';
     computerSelect();
     computerPlay = computerSelect();
+    setTimeout(cpuIcon, '500');
     round(playerPlay, computerPlay);
-    console.log(roundOutcome);
-    playerScore.textContent = playerJavaScore;
-    computerScore.textContent = computerJavaScore;
-    gameCheck();
+    setTimeout(() => {
+        console.log(roundOutcome);
+        playerScore.textContent = playerJavaScore;
+        computerScore.textContent = computerJavaScore;
+        gameCheck();
+    }, '1010');
 });
 
+gameStart.addEventListener('click', () => {
+    rockOff.style.display = 'none';
+    rockButton.style.display = 'block';
+    scissorsOff.style.display = 'none';
+    scissorsButton.style.display = 'block';
+    gameStart.style.display = 'none';
+    paperButton.style.display = 'block';
+    windows.style.backgroundColor = '#363636';
+    rps.style.display = 'none';
+    game.style.display = 'flex';
+})
 
-// startGame.addEventListener('click', game); 
+gameRestart.addEventListener('click', () => {
+    windows.style.backgroundColor = '#363636';
+    won.style.display = 'none';
+    lost.style.display = 'none';
+    game.style.display = 'flex';
+    rockOff.style.display = 'none';
+    rockButton.style.display = 'block';
+    scissorsOff.style.display = 'none';
+    scissorsButton.style.display = 'block';
+    gameRestart.style.display = 'none';
+    paperButton.style.display = 'block';
+    turnOffIcons();
+    playerJavaScore = 0;
+    computerJavaScore = 0;
+    playerScore.textContent = playerJavaScore;
+    computerScore.textContent = computerJavaScore;
+})
