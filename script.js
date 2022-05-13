7ea648eaa6dc260c20d8f732c73cdb2d00550536
JohnSlovak <jslovak97@gmail.com>
2022-05-13 15:46:56 +0200

    added basic ui elements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function computerPlay() {
+function computerSelect() {
     let num = Math.floor(Math.random() * 3);
     switch(num) {
         case 0:
@@ -13,14 +13,11 @@ function computerPlay() {
     }
 }
 
-function playerPlay() {
-    let num = prompt('Choose ROCK, PAPER, or SCISSORS', '');
-    num = num.toUpperCase();
-    return num;
-}
+let computerPlay = '';
+let playerPlay = '';
 
-let playerScore = 0;
-let computerScore = 0;
+let playerJavaScore = 0;
+let computerJavaScore = 0;
 let roundOutcome = '';
 
 function round(funA, funB) {
@@ -30,29 +27,87 @@ function round(funA, funB) {
     (funA == 'ROCK' && funB == 'SCISSORS') || 
     (funA == 'PAPER' && funB == 'ROCK') || 
     (funA == 'SCISSORS' && funB == 'PAPER')) {
-        playerScore++
+        playerJavaScore++
         roundOutcome = 'won'
     } else if (
     (funA == 'ROCK' && funB == 'PAPER') || 
     (funA == 'PAPER' && funB == 'SCISSORS') || 
     (funA == 'SCISSORS' && funB == 'ROCK')) {
-        computerScore++
+        computerJavaScore++
         roundOutcome = 'lost'
     }
 }
 
 
-function game() {
+/*function game() {
     for (''; playerScore < 5 && computerScore < 5; '') {
-        round(playerPlay(), computerPlay());
+        if 
 console.log('You : ' + roundOutcome);
 console.log('Your score: ' + playerScore);
 console.log('CPUs score: ' + computerScore);
     }
+}*/
+
+function gameCheck() {
+    if (playerJavaScore === 5) {
+        alert('You Won!');
+        restart();
+    } else if (computerJavaScore === 5) {
+        alert('You Lost!');
+        restart();
+    } else {
+        
+    }
 }
 
-game();
-/*round(playerPlay(), computerPlay()));
-console.log(roundOutcome)
-console.log(playerScore)
-console.log(computerScore)*/
+function restart() {
+    playerJavaScore = 0;
+    computerJavaScore = 0;
+    playerScore.textContent = playerJavaScore;
+    computerScore.textContent = computerJavaScore;
+}
+
+const rockButton = document.querySelector('.rock-button');
+const paperButton = document.querySelector('.paper-button');
+const scissorsButton = document.querySelector('.scissors-button');
+playerScore = document.querySelector('.player-score');
+computerScore = document.querySelector('.computer-score');
+const startGame = document.querySelector('start-game');
+const restartGame = document.querySelector('.restart');
+
+
+rockButton.addEventListener('click', () => {
+    playerPlay = 'ROCK';
+    computerSelect();
+    computerPlay = computerSelect();
+    round(playerPlay, computerPlay);
+    console.log(roundOutcome);
+    playerScore.textContent = playerJavaScore;
+    computerScore.textContent = computerJavaScore;
+    gameCheck();
+});
+
+paperButton.addEventListener('click', () => {
+    playerPlay = 'PAPER';
+    computerSelect();
+    computerPlay = computerSelect();
+    round(playerPlay, computerPlay);
+    console.log(roundOutcome);
+    playerScore.textContent = playerJavaScore;
+    computerScore.textContent = computerJavaScore;
+    gameCheck();
+});
+
+scissorsButton.addEventListener('click', () => {
+    playerPlay = 'SCISSORS';
+    computerSelect();
+    computerPlay = computerSelect();
+    round(playerPlay, computerPlay);
+    console.log(roundOutcome);
+    playerScore.textContent = playerJavaScore;
+    computerScore.textContent = computerJavaScore;
+    gameCheck();
+});
+
+
+// startGame.addEventListener('click', game); 
